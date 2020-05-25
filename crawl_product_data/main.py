885c9e7e1039715b49d74136dacab271ab07d26c
DucAnh <ngducanh1223@gmail.com>
2020-05-25 17:35:21 +0700

    Fix main

--- crawl_product_data/main.py
+++ crawl_product_data/main.py
@@ -3,8 +3,8 @@ from datetime import date
 
 today = date.today()
 file = "scrapy crawl shopee-item -o data/shopee_"+str(today)+".csv"
-# file = "scrapy crawl shopee-item -o data/tiki_"+str(today)+".csv"
-# file = "scrapy crawl shopee-item -o data/sendo_"+str(today)+".csv"
-# file = "scrapy crawl shopee-item -o data/lazada_"+str(today)+".csv"
+# file = "scrapy crawl tiki -o data/tiki_"+str(today)+".csv"
+# file = "scrapy crawl sendo -o data/sendo_"+str(today)+".csv"
+# file = "scrapy crawl lazada -o data/lazada_"+str(today)+".csv"
 
 cmdline.execute(file.split())
