bcfd79a1c1a13ec8278454b4af9e6384b9f72bff
DucAnh <ngducanh1223@gmail.com>
2020-06-04 15:49:33 +0700

    Update data, update tiki spider

--- crawl_product_data/crawl_product_data/spiders/tiki_spider.py
+++ crawl_product_data/crawl_product_data/spiders/tiki_spider.py
@@ -1,36 +1,50 @@
+import logging
 import scrapy
+import json
 
 
 ## crawl tiki
 class TikiSpider(scrapy.Spider):
     name = 'tiki'
-    '''
-    tiki: https://tiki.vn/noi/c10468
-    sendo: https://www.sendo.vn/giuong-cui-noi-cho-be/
-    shopee: https://shopee.vn/Xe-%C4%91%E1%BA%A9y-n%C3%B4i-c%C5%A9i-cat.163.2350.8851
-    lazada: https://www.lazada.vn/noi-tre-so-sinh/?spm=a2o4n.searchlistcategory.card.2.1fee29ebgMsiIB&item_id=309864813&from=onesearch_category_10657
-    lazada: https://www.lazada.vn/giuong-noi/?spm=a2o4n.searchlistcategory.card.6.1fee29ebRUgYLd&item_id=399328790&from=onesearch_category_10657
-    '''
     start_urls = [
         'https://tiki.vn/noi/c10468?page=1'
     ]
     page_number = 2
+    item_api = "https://tiki.vn/api/v2/products/{}"
+
+    def parse_item_api(self, response, product_name, final_price):
+        api_response = json.loads(response.text)
+        current_seller = api_response['current_seller']
+        shop_name = current_seller['name']
+        shop_id = current_seller['id']
+        product_id = api_response['id']
+        product_id2 = current_seller['product_id']
+        shop_url = current_seller['link']
+        product_url = "https://tiki.vn/" + api_response['url_path']
+
+        yield {
+            "product_id": product_id,
+            "product_url": product_url,
+            'name': product_name,
+            'price': final_price,
+            "shop_id": shop_id,
+            "shop_url": shop_url,
+            "shop_name": shop_name,
+            "product_id2": product_id2
+        }
 
     def parse(self, response):
         all_product_div = response.xpath('//div[contains(@class, \'product-box-list\')]/div')
-        # product_title = response.xpath('//div[contains(@class, \'product-box-list\')]/div/@data-title').extract()
-        # title = response.xpath('string(//div[contains(@class, \'product-box-list\')]/div/@data-title)').extract()
-        # final_price = response.css('.final-price::text').extract()
-        # final_price[0].strip()
-        # //div[contains(@class, 'product-box-list')]/div//span[@class='final-price']
         for product in all_product_div:
             product_title = product.xpath('@data-title').extract()[0]
+            product_id = product.xpath('@data-id').extract()[0]
             final_price = product.xpath('.//span[@class=\'final-price\']/text()').extract()[0].strip()
-            x = 0
-            yield {
-                'name': product_title,
-                'price': final_price
-            }
+
+            item_api_url = self.item_api.format(product_id)
+
+            yield scrapy.Request(item_api_url, callback=self.parse_item_api,
+                                 cb_kwargs={'product_name': product_title,
+                                            'final_price': final_price})
 
         next_page = 'https://tiki.vn/noi/c10468?page=' + str(TikiSpider.page_number)
         if TikiSpider.page_number <= 4:
