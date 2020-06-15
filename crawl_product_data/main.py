9fdd884aae57d51ffdc98bcb7df5c7b560c7909c
DucAnh <ngducanh1223@gmail.com>
2020-06-15 10:08:49 +0700

    data 15/6

--- crawl_product_data/main.py
+++ crawl_product_data/main.py
@@ -3,16 +3,13 @@ from datetime import date
 
 today = date.today()
 # file = "scrapy crawl shopee-item -o data/shopee/shopee_"+str(today)+".csv"
+file = "scrapy crawl shopee-item -o data/shopee/shopee_test.csv"
 # file = "scrapy crawl tiki -o data/tiki/tiki_"+str(today)+".csv"
 # file = "scrapy crawl sendo -o data/sendo/sendo_"+str(today)+".csv"
 # file = "scrapy crawl lazada -o data/lazada/lazada_"+str(today)+".csv"
 
-# file = "scrapy crawl shopee-item-all -o data/shopee/shopee_all_data.csv"
-# file = "scrapy crawl tiki-all -o data/tiki/tiki_all_data.csv"
-file = "scrapy crawl sendo-all -o data/sendo/sendo_all_data.csv"
+# file = "scrapy crawl shopee-item-all -o data/shopee/shopee_all_data"+str(today)+".csv"
+# file = "scrapy crawl tiki-all -o data/tiki/tiki_all_data"+str(today)+".csv"
+# file = "scrapy crawl sendo-all -o data/sendo/sendo_all_data"+str(today)+".csv"
 
-# file = "scrapy crawl shopee-item -o data/shopee/shopee_data.csv"
-# file = "scrapy crawl tiki -o data/tiki/tiki_data.csv"
-# file = "scrapy crawl sendo -o data/sendo/sendo_data.csv"
-# file = "scrapy crawl lazada -o data/lazada/lazada_data.csv"
 cmdline.execute(file.split())
